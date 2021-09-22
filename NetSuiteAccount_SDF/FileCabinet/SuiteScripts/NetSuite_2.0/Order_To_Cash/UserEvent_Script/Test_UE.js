976ffe873d9fd5606d91124ce733806c3912c670
jitendra-singh10 <jitendra.h.singh@accenture.com>
2021-09-22 10:33:41 +0530

    Commit

---
+++ NetSuiteAccount_SDF/FileCabinet/SuiteScripts/NetSuite_2.0/Order_To_Cash/UserEvent_Script/Test_UE.js
@@ -0,0 +1,29 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType UserEventScript
+ * @NModuleScope SameAccount
+ */
+define(['N/record'],
+
+function(record) {
+   
+  
+    function beforeLoad(scriptContext) {
+
+    }
+
+    function beforeSubmit(scriptContext) {
+
+    }
+
+    function afterSubmit(scriptContext) {
+
+    }
+
+    return {
+        beforeLoad: beforeLoad,
+        beforeSubmit: beforeSubmit,
+        afterSubmit: afterSubmit
+    };
+    
+});
